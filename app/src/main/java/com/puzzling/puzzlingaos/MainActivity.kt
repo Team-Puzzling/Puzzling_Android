c879b4564e9460fd2a334403fb24c1e83d3ac0ac
Cat-JiYoung <kimyamae82@gmail.com>
2023-06-25 19:46:58 +0900

    [FEAT] 프로젝트 생성 및 gitIgnore 설정

---
+++ app/src/main/java/com/puzzling/puzzlingaos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.puzzling.puzzlingaos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
