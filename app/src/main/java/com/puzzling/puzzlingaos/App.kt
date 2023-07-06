17a38b7ba64eedd2133ebf14869982989b12d7de
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-06 10:29:53 +0900

    [CHORE] 다크모드 비활성화

--- app/src/main/java/com/puzzling/puzzlingaos/App.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/App.kt
@@ -1,11 +1,13 @@
 package com.puzzling.puzzlingaos
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
