24a0e4badb03f9621c191ce35f2d195a55b0ea5d
emjayMJkim <minjeong030420@gmail.com>
2023-07-10 18:54:19 +0900

    [ADD] MVVM용 파일 생성

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/register/RegisterActivity.kt
@@ -1,13 +1,18 @@
 package com.puzzling.puzzlingaos.presentation.register
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseActivity
 import com.puzzling.puzzlingaos.databinding.ActivityRegisterBinding
+import com.puzzling.puzzlingaos.util.ViewModelFactory
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity_register) {
+
+    private val viewModel: RegisterViewModel by viewModels { ViewModelFactory(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding.viewModel = viewModel
     }
 }
