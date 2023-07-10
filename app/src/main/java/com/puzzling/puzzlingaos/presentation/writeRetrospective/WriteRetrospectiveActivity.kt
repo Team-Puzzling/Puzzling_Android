e273b987a615030a3ad257f940a886e7d0230e77
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-11 04:07:57 +0900

    [FEAT] 회고 선택 바텀시트 xml 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/writeRetrospective/WriteRetrospectiveActivity.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/writeRetrospective/WriteRetrospectiveActivity.kt
@@ -1,6 +1,7 @@
 package com.puzzling.puzzlingaos.presentation.writeRetrospective
 
 import android.os.Bundle
+import android.util.Log
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseActivity
 import com.puzzling.puzzlingaos.databinding.ActivityWriteRetrospectiveBinding
@@ -9,6 +10,23 @@ class WriteRetrospectiveActivity :
     BaseActivity<ActivityWriteRetrospectiveBinding>(R.layout.activity_write_retrospective) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write_retrospective)
+        clickBtn()
+    }
+
+    private fun clickBtn() {
+        binding.clWriteChip.setOnClickListener {
+            Log.d("write", "회고 선택 바텀시트 생겨야함")
+            val chooseRetrospectiveFragment = ChooseRetrospectiveFragment()
+            chooseRetrospectiveFragment.show(supportFragmentManager, "show")
+
+//            val bottomSheetView =
+//                layoutInflater.inflate(R.layout.fragment_choose_retrospective, null)
+//            val bottomSheetDialog = BottomSheetDialog(this)
+//            bottomSheetDialog.setContentView(bottomSheetView)
+        }
+
+        binding.tvWriteRegister.setOnClickListener {
+            Log.d("write", "저장 다이얼로그 표시함")
+        }
     }
 }
