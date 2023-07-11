343016d4372e647d059194c404e7f95a8b40fa73
gaeun <geun5744@gmail.com>
2023-07-11 15:13:52 +0900

    [ADD/#5] 프로필 입력 fragment 생성

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/main/invitationCode/InputProfileFragment.kt
@@ -0,0 +1,14 @@
+package com.puzzling.puzzlingaos.presentation.main.invitationCode
+
+import android.os.Bundle
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseFragment
+import com.puzzling.puzzlingaos.databinding.FragmentInputProfileBinding
+
+class InputProfileFragment :
+    BaseFragment<FragmentInputProfileBinding>(R.layout.fragment_input_profile) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
