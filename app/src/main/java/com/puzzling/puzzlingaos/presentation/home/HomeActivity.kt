0d731a6bebe9691b986edc2522c953184ab391cf
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-12 03:10:53 +0900

    [FEAT] 홈뷰 tabLayout bar 구현

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.puzzling.puzzlingaos.presentation.home
+
+import android.os.Bundle
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseActivity
+import com.puzzling.puzzlingaos.databinding.ActivityHomeBinding
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
