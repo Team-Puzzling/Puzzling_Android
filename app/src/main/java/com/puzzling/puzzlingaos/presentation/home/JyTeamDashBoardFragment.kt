aeeb8f42392ccfc71395246de3814508c888969a
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-12 05:23:09 +0900

    [FEAT] tab item 클릭시 framgent 교체 기능 추가

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/JyTeamDashBoardFragment.kt
@@ -0,0 +1,8 @@
+package com.puzzling.puzzlingaos.presentation.home
+
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseFragment
+import com.puzzling.puzzlingaos.databinding.FragmentJyTeamDashBoardBinding
+
+class JyTeamDashBoardFragment :
+    BaseFragment<FragmentJyTeamDashBoardBinding>(R.layout.fragment_jy_team_dash_board)
--- app/src/main/java/com/puzzling/puzzlingaos/presentation/main/MainActivity.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/main/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.puzzling.puzzlingaos.presentation.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.google.android.material.tabs.TabLayout
 import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseActivity
+import com.puzzling.puzzlingaos.databinding.ActivityMainBinding
+import com.puzzling.puzzlingaos.presentation.home.JyTeamDashBoardFragment
+import com.puzzling.puzzlingaos.presentation.home.PersonalBoardFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initTabLayout()
+        clickTabItem()
+    }
+
+    private fun initTabLayout() {
+        supportFragmentManager.findFragmentById(R.id.fl_home_main)
+            ?: navigateTo<PersonalBoardFragment>()
+    }
+
+    private fun clickTabItem() {
+        binding.tlHomeDashboard.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> navigateTo<PersonalBoardFragment>()
+                    1 -> navigateTo<JyTeamDashBoardFragment>()
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                // Not used
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                // Not used
+            }
+        })
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace(R.id.fl_home_main, T::class.java.newInstance())
+        }
     }
 }
