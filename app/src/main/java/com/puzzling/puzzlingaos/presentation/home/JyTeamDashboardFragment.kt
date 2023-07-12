3ed320ee3e33b0d666c1cf26a126bc82ab0da473
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-12 16:54:17 +0900

    [MERGE] rebase 충돌 해결

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/home/JyTeamDashboardFragment.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/JyTeamDashboardFragment.kt
@@ -2,7 +2,7 @@ package com.puzzling.puzzlingaos.presentation.home
 
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseFragment
-import com.puzzling.puzzlingaos.databinding.FragmentJyTeamDashboardBinding
+import com.puzzling.puzzlingaos.databinding.FragmentJyTeamDashBoardBinding
 
-class JyTeamDashboardFragment :
-    BaseFragment<FragmentJyTeamDashboardBinding>(R.layout.fragment_jy_team_dashboard)
+class JyTeamDashBoardFragment :
+    BaseFragment<FragmentJyTeamDashBoardBinding>(R.layout.fragment_jy_team_dash_board)
