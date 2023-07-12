a5019ac83de84bab32b3aa59b58839ad4c034513
gaeun <geun5744@gmail.com>
2023-07-12 21:43:33 +0900

    [RENAME/#11] Fragment name 수정

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/MyPageFragment.kt
@@ -0,0 +1,7 @@
+package com.puzzling.puzzlingaos.presentation.home
+
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseFragment
+import com.puzzling.puzzlingaos.databinding.FragmentMyPageBinding
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page)
