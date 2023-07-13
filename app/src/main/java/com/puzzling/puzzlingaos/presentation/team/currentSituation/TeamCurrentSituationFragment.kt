b9b672f348617d743303deb5cabab17660f95b6d
emjayMJkim <minjeong030420@gmail.com>
2023-07-13 21:07:30 +0900

    [ADD] TeamCurrentSituationFragment 생성

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/team/currentSituation/TeamCurrentSituationFragment.kt
@@ -0,0 +1,7 @@
+package com.puzzling.puzzlingaos.presentation.team.currentSituation
+
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseFragment
+import com.puzzling.puzzlingaos.databinding.FragmentHomeBinding
+
+class TeamCurrentSituationFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_team_current_situation)
