0046b8287e21edd329b736c09dfb96db6909f55e
gaeun <geun5744@gmail.com>
2023-07-14 02:49:53 +0900

    [FEAT/#11] 날짜 계산을 통한 디데이 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/MyProjectAdapter.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/MyProjectAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.puzzling.puzzlingaos.data.model.response.ResponseMyPageProjectDto
 import com.puzzling.puzzlingaos.databinding.ItemMyProjectBinding
 import com.puzzling.puzzlingaos.util.ItemDiffCallback
+import java.lang.Math.abs
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MyProjectAdapter() :
     ListAdapter<ResponseMyPageProjectDto, MyProjectAdapter.MyProjectViewHolder>(
@@ -26,9 +28,21 @@ class MyProjectAdapter() :
 
     class MyProjectViewHolder(private val binding: ItemMyProjectBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun onBind(item: ResponseMyPageProjectDto) {
             with(binding) {
                 tvMyPageProjectName.text = item.projectName
+
+                val now = Date()
+                val startDate =
+                    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(item.startDate)
+                val timeDiff = (now.time - startDate.time) / (24 * 60 * 60 * 1000)
+
+                if (timeDiff < 0) {
+                    binding.tvMyPageStartDate.text = "D - ${kotlin.math.abs(timeDiff - 1)}"
+                } else {
+                    binding.tvMyPageStartDate.text = "D + ${timeDiff.toString()}"
+                }
             }
         }
     }
