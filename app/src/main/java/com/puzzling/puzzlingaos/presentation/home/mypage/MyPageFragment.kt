3122b8cae9920014d35e5c2fb1309977941a906e
gaeun <geun5744@gmail.com>
2023-07-13 21:34:48 +0900

    [FEAT/#11] 마이페이지 회고 리스트 Adapter 연결

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/MyPageFragment.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/MyPageFragment.kt
@@ -1,7 +1,36 @@
 package com.puzzling.puzzlingaos.presentation.home.mypage
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseFragment
+import com.puzzling.puzzlingaos.data.model.response.ResponseMyPageProjectDto
 import com.puzzling.puzzlingaos.databinding.FragmentMyPageBinding
 
-class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page)
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    private val dummyItemList = mutableListOf<ResponseMyPageProjectDto>(
+        ResponseMyPageProjectDto("Piickle", "2023-07-03", 2),
+        ResponseMyPageProjectDto("HARA", "2023-07-28", 3),
+        ResponseMyPageProjectDto("낫투두", "2023-07-12", 4),
+        ResponseMyPageProjectDto("PEEKABOOK", "2023-07-20", 5),
+        ResponseMyPageProjectDto("킵고잇", "2023-06-25", 8),
+        ResponseMyPageProjectDto("킵고잇", "2023-06-25", 8),
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        val myProjectAdapter = MyProjectAdapter()
+        with(binding) {
+            rcvMyPageMain.adapter = myProjectAdapter
+            rcvMyPageMain.layoutManager = LinearLayoutManager(activity)
+        }
+        myProjectAdapter.submitList(dummyItemList)
+    }
+}
