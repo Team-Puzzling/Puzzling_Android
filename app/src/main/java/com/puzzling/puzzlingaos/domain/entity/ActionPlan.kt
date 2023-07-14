9b1eafcddaeabd4a364f52ed8b0fef6b3eb08887
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-14 18:12:42 +0900

    [MERGE] pull 충돌 해결

--- app/src/main/java/com/puzzling/puzzlingaos/domain/entity/ActionPlan.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/domain/entity/ActionPlan.kt
@@ -1,16 +1,12 @@
 package com.puzzling.puzzlingaos.domain.entity
 
-/*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 @Serializable
 data class ActionPlan(
     @SerialName("actionPlanContent")
     val actionPlanContent: String,
     @SerialName("actionPlanDate")
-    val actionPlanDate: String?,
-)
- */
-
-data class ActionPlan(
-    val actionPlanContent: String,
-    val actionPlanDate: String?,
+    val actionPlanDate: String,
 )
