6e7c231e89a598d05a5d218827fbe6c489b29145
gaeun <geun5744@gmail.com>
2023-07-14 19:17:28 +0900

    [FEAT/#11] 버튼 클릭시 bottomSheet 생성 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/adapter/MyRetroTopAdapter.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/adapter/MyRetroTopAdapter.kt
@@ -1,6 +1,7 @@
 package com.puzzling.puzzlingaos.presentation.home.mypage.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.puzzling.puzzlingaos.databinding.ItemMyretroProjectBinding
@@ -8,8 +9,10 @@ import com.puzzling.puzzlingaos.databinding.ItemMyretroProjectBinding
 class MyRetroTopAdapter(private val projectName: String) :
     RecyclerView.Adapter<MyRetroTopAdapter.MyRetroTopViewHolder>() {
 
+    private var listener: OnItemClickListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyRetroTopViewHolder {
-        val binding : ItemMyretroProjectBinding = ItemMyretroProjectBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding: ItemMyretroProjectBinding = ItemMyretroProjectBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyRetroTopViewHolder(binding)
     }
 
@@ -21,10 +24,24 @@ class MyRetroTopAdapter(private val projectName: String) :
         holder.onBind(projectName)
     }
 
-    class MyRetroTopViewHolder(private val binding: ItemMyretroProjectBinding) :
+    inner class MyRetroTopViewHolder(private val binding: ItemMyretroProjectBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(projectName: String) {
             binding.tvMyretroProjectName.text = projectName
+
+            if (bindingAdapterPosition != RecyclerView.NO_POSITION) {
+                binding.containerMyPageProject.setOnClickListener {
+                    listener?.onItemClick(itemView, projectName, bindingAdapterPosition)
+                }
+            }
         }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(v: View, data: String, pos: Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
     }
 }
