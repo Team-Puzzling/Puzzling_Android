a592988cc27b040ed240f97557e0d4d6655c117f
gaeun <geun5744@gmail.com>
2023-07-14 17:39:45 +0900

    [ADD/#11] ChooseProjectBottomFragment.kt 생성

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/mypage/ChooseProjectBottomFragment.kt
@@ -0,0 +1,14 @@
+package com.puzzling.puzzlingaos.presentation.home.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BottomSheetDialogFragment
+import com.puzzling.puzzlingaos.databinding.FragmentBottomChooseProjectBinding
+
+class ChooseProjectBottomFragment : BottomSheetDialogFragment<FragmentBottomChooseProjectBinding>(R.layout.fragment_bottom_choose_project) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
