bb1953a9d70ed6130086e4cd1933e6751d2b2403
gaeun <geun5744@gmail.com>
2023-07-15 03:22:08 +0900

    [ADD/#23] 회고 상세조회 activity 생성

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroActivity.kt
@@ -0,0 +1,13 @@
+package com.puzzling.puzzlingaos.presentation.detailRetrospect
+
+import android.os.Bundle
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseActivity
+import com.puzzling.puzzlingaos.databinding.ActivityDetailRetroBinding
+
+class DetailRetroActivity :
+    BaseActivity<ActivityDetailRetroBinding>(R.layout.activity_detail_retro) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
