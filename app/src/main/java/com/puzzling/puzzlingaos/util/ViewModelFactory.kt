273114d2007f3f8d6605bdd13c83da52bb4cba75
gaeun <geun5744@gmail.com>
2023-07-16 03:28:55 +0900

    [FEAT/#23] invitationCode viewModelFactory 적용

--- app/src/main/java/com/puzzling/puzzlingaos/util/ViewModelFactory.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/util/ViewModelFactory.kt
@@ -5,13 +5,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.puzzling.puzzlingaos.data.repository.RegisterRepositoryImpl
 import com.puzzling.puzzlingaos.data.source.remote.RegisterRemoteDataSource
+import com.puzzling.puzzlingaos.presentation.invitationCode.InvitationCodeViewModel
 import com.puzzling.puzzlingaos.presentation.register.RegisterViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
-    override fun<T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
-            return RegisterViewModel(RegisterRepositoryImpl(RegisterRemoteDataSource())) as T
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(RegisterRepositoryImpl(RegisterRemoteDataSource())) as T
+            }
+            modelClass.isAssignableFrom(InvitationCodeViewModel::class.java) -> {
+                InvitationCodeViewModel(context) as T
+            }
+
+            else -> {
+                throw java.lang.IllegalArgumentException("Unknown ViewModel")
+            }
         }
-        throw java.lang.IllegalArgumentException("Unknown ViewModel")
     }
 }
