c2e8e59ebb8814f3e5fba47a5d3debcd490c6174
gaeun <geun5744@gmail.com>
2023-07-15 16:55:28 +0900

    [UI/#23] 선택된 cell 파란 oval background 적용

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroActivity.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroActivity.kt
@@ -1,47 +1,42 @@
 package com.puzzling.puzzlingaos.presentation.detailRetrospect
 
+import android.app.ActionBar.LayoutParams
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseActivity
 import com.puzzling.puzzlingaos.databinding.ActivityDetailRetroBinding
-import kotlin.math.roundToInt
 
 class DetailRetroActivity :
     BaseActivity<ActivityDetailRetroBinding>(R.layout.activity_detail_retro) {
 
-    private val tabTitle = mutableMapOf(
-        "5" to R.drawable.ic_monday,
-        "6" to R.drawable.ic_thuesday,
-        "7" to R.drawable.ic_monday,
-        "8" to R.drawable.ic_thuesday,
-        "9" to R.drawable.ic_monday,
-        "10" to R.drawable.ic_thuesday,
-        "11" to R.drawable.ic_monday,
-
+    private val tabTitle = arrayListOf(
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val titles = ArrayList(tabTitle.keys)
         binding.viewPager.adapter = WeekTapAdapter(this)
-        TabLayoutMediator(binding.tablayout, binding.viewPager) { tab, position ->
-            tab.text = titles[position]
+        TabLayoutMediator(binding.tlDetailRetroDate, binding.viewPager) { tab, position ->
+            tab.text = tabTitle[position]
         }.attach()
 
-        tabTitle.values.forEachIndexed { index, imageResId ->
-            val textView = LayoutInflater.from(this).inflate(R.layout.tab_title, null) as TextView
-            textView.setCompoundDrawablesWithIntrinsicBounds(0, imageResId, 0, 0)
-            textView.compoundDrawablePadding = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                4f,
-                resources.displayMetrics,
-            ).roundToInt()
-            binding.tablayout.getTabAt(index)?.customView = textView
+        for (i in 0..7) {
+            val textView = LayoutInflater.from(this).inflate(R.layout.tab_title, null) as ConstraintLayout
+            // textView.layoutParams = ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+            binding.tlDetailRetroDate.getTabAt(i)?.customView = textView
         }
     }
 }
