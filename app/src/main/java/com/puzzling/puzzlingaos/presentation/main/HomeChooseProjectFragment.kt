58cf99af44817b2ae919c22eca95a3675bbbd10c
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-15 18:08:28 +0900

    [FEAT] 홈 프로젝트 바텀시트 클릭 이벤트 추가

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/main/HomeChooseProjectFragment.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/main/HomeChooseProjectFragment.kt
@@ -1,11 +1,14 @@
 package com.puzzling.puzzlingaos.presentation.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BottomSheetDialogFragment
 import com.puzzling.puzzlingaos.data.model.response.ResponseMyPageProjectDto
 import com.puzzling.puzzlingaos.databinding.FragmentHomeChooseProjectBinding
+import com.puzzling.puzzlingaos.presentation.invitationCode.InvitationCodeActivity
+import com.puzzling.puzzlingaos.presentation.register.RegisterActivity
 
 class HomeChooseProjectFragment :
     BottomSheetDialogFragment<FragmentHomeChooseProjectBinding>(R.layout.fragment_home_choose_project) {
@@ -29,11 +32,29 @@ class HomeChooseProjectFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBottomSheetAdapter()
+        clickBottomBtn()
     }
 
     private fun setBottomSheetAdapter() {
         val chooseProjectAdapter = HomeChooseProjectAdapter("PEEKABOOK")
         binding.rcvHomeChooseProject.adapter = chooseProjectAdapter
         chooseProjectAdapter.setItemList(dummyItemList)
+    }
+
+    private fun clickBottomBtn() {
+        with(binding) {
+            clHomeChooseProject.setOnClickListener {
+                activity?.let {
+                    val intent = Intent(context, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            clHomeJoinProject.setOnClickListener {
+                activity?.let {
+                    val intent = Intent(context, InvitationCodeActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
