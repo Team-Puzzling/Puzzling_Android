97d91768a0b3fdc050ccdf56c2dd1164984c9b19
emjayMJkim <minjeong030420@gmail.com>
2023-07-16 00:46:02 +0900

    [FEAT] 일주일 날짜 상단 띄우기 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/team/currentSituation/RetrospectThisWeekAdapter.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/team/currentSituation/RetrospectThisWeekAdapter.kt
@@ -1,12 +1,16 @@
 package com.puzzling.puzzlingaos.presentation.team.currentSituation
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.puzzling.puzzlingaos.databinding.ItemTeamThisWeekBinding
 import com.puzzling.puzzlingaos.domain.entity.ThisWeek
 import com.puzzling.puzzlingaos.util.ItemDiffCallback
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 class RetrospectThisWeekAdapter(private val viewModel: TeamCurrentSituationViewModel) : ListAdapter<ThisWeek, RetrospectThisWeekAdapter.RetrospectThisWeekViewHolder>(
     ItemDiffCallback<ThisWeek>(
@@ -15,15 +19,15 @@ class RetrospectThisWeekAdapter(private val viewModel: TeamCurrentSituationViewM
     ),
 ) {
 
-    private var itemList = mutableListOf<ThisWeek>(
-        ThisWeek("3"),
-        ThisWeek("4"),
-        ThisWeek("5"),
-        ThisWeek("6"),
-        ThisWeek("7"),
-        ThisWeek("8"),
-        ThisWeek("9"),
-
+    @RequiresApi(Build.VERSION_CODES.O)
+    private var itemList = mutableListOf(
+        ThisWeek(getWeekDatesWithToday()[0].dayOfMonth.toString()),
+        ThisWeek(getWeekDatesWithToday()[1].dayOfMonth.toString()),
+        ThisWeek(getWeekDatesWithToday()[2].dayOfMonth.toString()),
+        ThisWeek(getWeekDatesWithToday()[3].dayOfMonth.toString()),
+        ThisWeek(getWeekDatesWithToday()[4].dayOfMonth.toString()),
+        ThisWeek(getWeekDatesWithToday()[5].dayOfMonth.toString()),
+        ThisWeek(getWeekDatesWithToday()[6].dayOfMonth.toString()),
     )
 
     inner class RetrospectThisWeekViewHolder(private val binding: ItemTeamThisWeekBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -37,9 +41,28 @@ class RetrospectThisWeekAdapter(private val viewModel: TeamCurrentSituationViewM
         return RetrospectThisWeekViewHolder(binding)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: RetrospectThisWeekViewHolder, position: Int) {
         holder.bind(itemList[position])
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getItemCount(): Int = itemList.size
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getWeekDatesWithToday(): List<LocalDate> {
+        val today = LocalDate.now()
+        val startOfWeek = today.with(DayOfWeek.MONDAY) // 해당 주의 시작일
+        val endOfWeek = today.with(DayOfWeek.SUNDAY) // 해당 주의 종료일
+
+        val dates = mutableListOf<LocalDate>()
+        var currentDate = startOfWeek
+
+        while (!currentDate.isAfter(endOfWeek)) {
+            dates.add(currentDate)
+            currentDate = currentDate.plusDays(1)
+        }
+
+        return dates
+    }
 }
