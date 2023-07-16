5369d0cbeccaf0022d7a10ce7d68a9f9c0d7f0b8
gaeun <geun5744@gmail.com>
2023-07-17 00:29:49 +0900

    [FIX/#23] fragment 적용 방식 수정

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/WeekTapAdapter.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/WeekTapAdapter.kt
@@ -3,22 +3,23 @@ package com.puzzling.puzzlingaos.presentation.detailRetrospect
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.puzzling.puzzlingaos.data.model.response.ResponseDetailRetroDto
 
-class WeekTapAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+class WeekTapAdapter(
+    fragmentActivity: FragmentActivity,
+    private val dataList: List<ResponseDetailRetroDto>,
+    private val num: MutableList<Int>,
+) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
         return 7
     }
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> DetailRetroFragment("5")
-            1 -> DetailRetroFragment("6")
-            2 -> DetailRetroFragment("7")
-            3 -> DetailRetroFragment("8")
-            4 -> DetailRetroFragment("9")
-            5 -> DetailRetroFragment("10")
-            6 -> DetailRetroFragment("11")
-            else -> DetailRetroFragment("5")
+        return when (num[position]) {
+            0 -> NoDetailRetroFragment()
+            1 -> NoDetailRetroFragment()
+            2 -> DetailRetroFragment(position)
+            else -> DetailRetroFragment(position)
         }
     }
 }
