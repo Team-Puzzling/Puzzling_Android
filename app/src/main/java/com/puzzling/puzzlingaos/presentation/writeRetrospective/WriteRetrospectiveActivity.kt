113bac5b9eea341420ee488119196f0154cfa63a
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-16 21:54:46 +0900

    [FEAT] editText error message 처리

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/writeRetrospective/WriteRetrospectiveActivity.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/writeRetrospective/WriteRetrospectiveActivity.kt
@@ -2,15 +2,25 @@ package com.puzzling.puzzlingaos.presentation.writeRetrospective
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.MutableLiveData
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseActivity
 import com.puzzling.puzzlingaos.databinding.ActivityWriteRetrospectiveBinding
 
 class WriteRetrospectiveActivity :
     BaseActivity<ActivityWriteRetrospectiveBinding>(R.layout.activity_write_retrospective) {
+    private val viewModel by viewModels<WriteRetrospectiveViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+
         clickBtn()
+        textInputListener(viewModel.question1)
+        textInputListener(viewModel.question2)
+        textInputListener(viewModel.question3)
+        validBtnEnabled()
     }
 
     private fun clickBtn() {
@@ -34,7 +44,52 @@ class WriteRetrospectiveActivity :
         registerDialog.show(supportFragmentManager, TAG_REGISTER_DIALOG)
     }
 
+    private fun textInputListener(textBox: MutableLiveData<String>) {
+        textBox.observe(this) { textBoxString ->
+            viewModel.let { viewModel ->
+                if (!viewModel.validTextBox(textBoxString)) {
+                    when (textBox) {
+                        viewModel.question1 -> {
+                            binding.edtWriteFirstMain.error = ERROR_MESSAGE
+                            viewModel.isValidquestion1.value = false
+                        }
+                        viewModel.question2 -> {
+                            binding.edtWriteSecondMain.error = ERROR_MESSAGE
+                            viewModel.isValidquestion2.value = false
+                        }
+                        viewModel.question3 -> {
+                            binding.edtWriteThirdMain.error = ERROR_MESSAGE
+                            viewModel.isValidquestion3.value = false
+                        }
+                    }
+                } else {
+                    when (textBox) {
+                        viewModel.question1 -> {
+                            binding.edtWriteFirstMain.error = null
+                            viewModel.isValidquestion1.value = true
+                        }
+                        viewModel.question2 -> {
+                            binding.edtWriteSecondMain.error = null
+                            viewModel.isValidquestion2.value = true
+                        }
+                        viewModel.question3 -> {
+                            binding.edtWriteSecondMain.error = null
+                            viewModel.isValidquestion2.value = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun validBtnEnabled() {
+        viewModel.isEnabledRegister.observe(this) {
+            viewModel.checkBtnEnabled()
+        }
+    }
+
     companion object {
         const val TAG_REGISTER_DIALOG = "REGISTER_DIALOG"
+        const val ERROR_MESSAGE = "이모지를 사용할 수 없어요"
     }
 }
--- app/src/main/java/com/puzzling/puzzlingaos/presentation/writeRetrospective/WriteRetrospectiveViewModel.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/writeRetrospective/WriteRetrospectiveViewModel.kt
@@ -1,7 +1,50 @@
 package com.puzzling.puzzlingaos.presentation.writeRetrospective
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.puzzling.puzzlingaos.presentation.register.RegisterViewModel
 
-class WriteRetrospectiveViewModel : ViewModel()
+class WriteRetrospectiveViewModel : ViewModel() {
+    private val registerRegex = RegisterViewModel.REGISTER_REGEX.toRegex()
+
+    val question1 = MutableLiveData<String>()
+    val question2 = MutableLiveData<String>()
+    val question3 = MutableLiveData<String>()
+
+    fun validTextBox(textBox: String): Boolean {
+        return textBox.matches(registerRegex)
+    }
+
+    var isValidquestion1: MutableLiveData<Boolean> = MutableLiveData(true)
+    var isValidquestion2: MutableLiveData<Boolean> = MutableLiveData(true)
+    var isValidquestion3: MutableLiveData<Boolean> = MutableLiveData(true)
+
+    fun validRegister(textBox: String): Boolean {
+        return textBox.matches(registerRegex) && !textBox.isNullOrBlank()
+    }
+
+    fun isValid(): Boolean {
+        return validRegister(question1.value.orEmpty()) &&
+            validRegister(question2.value.orEmpty()) &&
+            validRegister(question3.value.orEmpty())
+    }
+
+    val isEnabledRegister = MediatorLiveData<Boolean>().apply {
+        addSource(question1) { value = isValid() }
+        addSource(question2) { value = isValid() }
+        addSource(question3) { value = isValid() }
+    }
+    var isBtnEnabled: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    fun checkBtnEnabled() {
+        isBtnEnabled.value = isValid()
+    }
+
+    companion object {
+        const val WRITE_REGEX =
+            "/([\\u2700-\\u27BF]|[\\uE000-\\uF8FF]|\\uD83C[\\uDC00-\\uDFFF]|\\uD83D[\\uDC00-\\uDFFF]|[\\u2011-\\u26FF]|\\uD83E[\\uDD10-\\uDDFF])/g"
+    }
+}
 // 특수 문자 가능
 // 이모지 사용시 빨간 라인 뜸
