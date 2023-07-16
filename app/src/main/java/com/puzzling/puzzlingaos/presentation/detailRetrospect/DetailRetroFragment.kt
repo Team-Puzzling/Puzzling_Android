46d6f54f15a24ad1f6451e2080929ee758b3161c
gaeun <geun5744@gmail.com>
2023-07-17 03:42:34 +0900

    [FEAT/#23] 상세회고 조회 recyclerview 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroFragment.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroFragment.kt
@@ -1,10 +1,12 @@
 package com.puzzling.puzzlingaos.presentation.detailRetrospect
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseFragment
+import com.puzzling.puzzlingaos.data.model.response.ResponseDetailRetroDto
 import com.puzzling.puzzlingaos.databinding.FragmentDetailRetroBinding
 import com.puzzling.puzzlingaos.util.ViewModelFactory
 
@@ -14,6 +16,7 @@ class DetailRetroFragment(
     BaseFragment<FragmentDetailRetroBinding>(R.layout.fragment_detail_retro) {
 
     private lateinit var viewModel: DetailRetroViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -25,10 +28,13 @@ class DetailRetroFragment(
         val day = viewModel.week[position]
         val dataList = viewModel.detailRetroList.value
 
+        val detailRetroAdapter = DetailRetroAdapter()
+
         if (dataList != null) {
             for (data in dataList) {
                 if (day == data.reviewDay) {
-                    binding.tvTest.text = data?.contents?.get(1)?.title
+                    binding.rcvDetailRetroMain.adapter = detailRetroAdapter
+                    detailRetroAdapter.submitList(data.contents)
                 }
             }
         }
--- app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroViewModel.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroViewModel.kt
@@ -29,9 +29,24 @@ class DetailRetroViewModel(context: Context) : ViewModel() {
             "화",
             "2023-07-18",
             listOf(
-                ResponseDetailRetroDto.Content("잘한점...", "잘한점 어쩌구...."),
-                ResponseDetailRetroDto.Content("아쉬운점...", "아쉬운점 어쩌구...."),
-                ResponseDetailRetroDto.Content("배운점...", "배운점 어쩌구...."),
+                ResponseDetailRetroDto.Content(
+                    "잘한점...",
+                    "placeholder 위치, 텍스트박스는 이 사이즈가 기본 사이즈입니다. " +
+                        "양 옆 마진 16px을 가진 텍스트박스가 존재합니다. 회고를 길게길게 작성하여 내용이 더욱 길어지면 다음과 같이 작성한 회고 길이에 맞게 길어지게 됩니다. " +
+                        "길어지고 길어지고 빙빙 돌아가는 회전목마처럼 어쩌고 ",
+                ),
+                ResponseDetailRetroDto.Content(
+                    "아쉬운점...",
+                    "placeholder 위치, 텍스트박스는 이 사이즈가 기본 사이즈입니다. " +
+                        "양 옆 마진 16px을 가진 텍스트박스가 존재합니다. 회고를 길게길게 작성하여 내용이 더욱 길어지면 다음과 같이 작성한 회고 길이에 맞게 길어지게 됩니다. " +
+                        "길어지고 길어지고 빙빙 돌아가는 회전목마처럼 어쩌고 ",
+                ),
+                ResponseDetailRetroDto.Content(
+                    "배운점...",
+                    "placeholder 위치, 텍스트박스는 이 사이즈가 기본 사이즈입니다. " +
+                        "양 옆 마진 16px을 가진 텍스트박스가 존재합니다. 회고를 길게길게 작성하여 내용이 더욱 길어지면 다음과 같이 작성한 회고 길이에 맞게 길어지게 됩니다. " +
+                        "길어지고 길어지고 빙빙 돌아가는 회전목마처럼 어쩌고 ",
+                ),
             ),
         ),
         ResponseDetailRetroDto(
