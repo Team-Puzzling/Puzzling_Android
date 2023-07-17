015039de383524e653a446744e63fd400baeb734
gaeun <geun5744@gmail.com>
2023-07-18 01:47:07 +0900

    [UI/#1] 로그인 뷰 UI 구현

---
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/onboarding/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.puzzling.puzzlingaos.presentation.onboarding
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.puzzling.puzzlingaos.R
+import com.puzzling.puzzlingaos.base.BaseActivity
+import com.puzzling.puzzlingaos.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login){
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
+}
