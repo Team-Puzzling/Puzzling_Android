5bdecf672e60c077ff796e218b0197a8e903fa2b
gaeun <geun5744@gmail.com>
2023-07-18 01:46:38 +0900

    [CHORE/#1] 카카오 로그인 기초 setting

--- app/src/main/java/com/puzzling/puzzlingaos/App.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/App.kt
@@ -2,12 +2,15 @@ package com.puzzling.puzzlingaos
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.KakaoSdk
+import com.puzzling.puzzlingaos.BuildConfig.NATIVE_APP_KEY
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, NATIVE_APP_KEY)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
