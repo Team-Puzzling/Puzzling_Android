b89a2945dbb658d3e5fb5df5c5b3755f19397f87
gaeun <geun5744@gmail.com>
2023-07-18 01:50:48 +0900

    [FEAT/#1] SharedPreference 내부 DB 구현

--- app/src/main/java/com/puzzling/puzzlingaos/App.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.puzzling.puzzlingaos.BuildConfig.NATIVE_APP_KEY
+import com.puzzling.puzzlingaos.data.source.local.LocalDataSource
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,6 +12,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, NATIVE_APP_KEY)
+        LocalDataSource.init(this)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
