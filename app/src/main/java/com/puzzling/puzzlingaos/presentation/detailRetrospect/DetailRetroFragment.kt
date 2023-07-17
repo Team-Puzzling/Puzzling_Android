b7c4e18686a4466869093370b94ce39198c71eb3
gaeun <geun5744@gmail.com>
2023-07-17 14:43:43 +0900

    [FEAT/#23] recyclerview 사이 선 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroFragment.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/detailRetrospect/DetailRetroFragment.kt
@@ -1,13 +1,13 @@
 package com.puzzling.puzzlingaos.presentation.detailRetrospect
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+// import androidx.recyclerview.widget.DividerItemDecoration
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseFragment
-import com.puzzling.puzzlingaos.data.model.response.ResponseDetailRetroDto
 import com.puzzling.puzzlingaos.databinding.FragmentDetailRetroBinding
+import com.puzzling.puzzlingaos.util.DividerItemDecoration
 import com.puzzling.puzzlingaos.util.ViewModelFactory
 
 class DetailRetroFragment(
@@ -29,6 +29,13 @@ class DetailRetroFragment(
         val dataList = viewModel.detailRetroList.value
 
         val detailRetroAdapter = DetailRetroAdapter()
+        // binding.rcvDetailRetroMain.addItemDecoration(DividerItemDecoration(context, LinearLayout.VERTICAL))
+        binding.rcvDetailRetroMain.addItemDecoration(
+            com.puzzling.puzzlingaos.util.DividerItemDecoration(
+                R.color.gray_300,
+                32,
+            ),
+        )
 
         if (dataList != null) {
             for (data in dataList) {
