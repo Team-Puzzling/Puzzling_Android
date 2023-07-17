78f24b12ce03f85b363ad17994cf17da8eed2d8e
gaeun <geun5744@gmail.com>
2023-07-18 02:54:36 +0900

    [ADD/#1] ViewModelFactory에 LoginViewModel 추가

--- app/src/main/java/com/puzzling/puzzlingaos/util/ViewModelFactory.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/util/ViewModelFactory.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.puzzling.puzzlingaos.data.repository.RegisterRepositoryImpl
+import com.puzzling.puzzlingaos.data.service.KakaoLoginService
 import com.puzzling.puzzlingaos.data.source.remote.RegisterRemoteDataSource
 import com.puzzling.puzzlingaos.presentation.detailRetrospect.DetailRetroViewModel
 import com.puzzling.puzzlingaos.presentation.invitationCode.InvitationCodeViewModel
+import com.puzzling.puzzlingaos.presentation.onboarding.LoginViewModel
 import com.puzzling.puzzlingaos.presentation.register.RegisterViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
@@ -21,6 +23,11 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
 
             modelClass.isAssignableFrom(DetailRetroViewModel::class.java) -> {
                 DetailRetroViewModel(context) as T
+            }
+
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                val repository = KakaoLoginService(context)
+                LoginViewModel(repository) as T
             }
 
             else -> {
