3580593e9a4a8f34de4823dc6d2f4c539008479d
emjayMJkim <minjeong030420@gmail.com>
2023-07-18 19:17:45 +0900

    [ADD] 프로젝트 등록 data class 생성

---
+++ app/src/main/java/com/puzzling/puzzlingaos/data/model/response/ResponseProjectRegisterDto.kt
@@ -0,0 +1,22 @@
+package com.puzzling.puzzlingaos.data.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseProjectRegisterDto(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: ProjectCode,
+) {
+    @Serializable
+    data class ProjectCode(
+        @SerialName("projectCode")
+        val projectCode: String,
+    )
+}
