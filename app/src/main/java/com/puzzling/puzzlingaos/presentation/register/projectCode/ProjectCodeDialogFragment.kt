b208910c3e6093c4728d7d9ab0437ada5a0751dc
emjayMJkim <minjeong030420@gmail.com>
2023-07-19 09:21:03 +0900

    [MOD] 필요 없는 코드 수정

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/register/projectCode/ProjectCodeDialogFragment.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/register/projectCode/ProjectCodeDialogFragment.kt
@@ -2,21 +2,16 @@ package com.puzzling.puzzlingaos.presentation.register.projectCode
 
 import android.content.ClipData
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.activity.viewModels
-import androidx.fragment.app.viewModels
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseDialogFragment
 import com.puzzling.puzzlingaos.databinding.FragmentProjectcodeDialogBinding
-import com.puzzling.puzzlingaos.presentation.register.RegisterViewModel
 import com.puzzling.puzzlingaos.util.getClipboardManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProjectCodeDialogFragment(private val code: String) : BaseDialogFragment<FragmentProjectcodeDialogBinding>(R.layout.fragment_projectcode_dialog) {
 
-    // private val viewModel by viewModels<>()
     private lateinit var clip: ClipData
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,7 +30,6 @@ class ProjectCodeDialogFragment(private val code: String) : BaseDialogFragment<F
         with(binding) {
             btnRegisterDialogTop.setOnClickListener {
                 copyText()
-                Log.d("hoho", "clip:: $clip")
             }
             btnRegisterDialogMiddle.setOnClickListener { }
             btnRegisterDialogBottom.setOnClickListener {
@@ -47,9 +41,7 @@ class ProjectCodeDialogFragment(private val code: String) : BaseDialogFragment<F
     private fun copyText() {
         val clipboard = requireContext().getClipboardManager()
         // 일반 텍스트 복사
-        clip =
-            ClipData.newPlainText("CLIPBOARD_TEXT", code)
-        Log.d("hoho", "Copied data: $code")
+        clip = ClipData.newPlainText("CLIPBOARD_TEXT", code)
         // ClipData 개체를 클립보드에 넣음
         clipboard.setPrimaryClip(clip)
     }
--- app/src/main/java/com/puzzling/puzzlingaos/util/GetClipBoardManger.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/util/GetClipBoardManger.kt
@@ -1,15 +1,8 @@
 package com.puzzling.puzzlingaos.util
 
-import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 
 fun Context.getClipboardManager(): ClipboardManager {
     return getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-}
-
-fun String.copyToClipboard(context: Context) {
-    val clipBoard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    val clipData = ClipData.newPlainText("COPY_TEXT", this)
-    clipBoard.setPrimaryClip(clipData)
 }
