c7d1b60d258eeac5400cedb3dbb3a9ef1f195d1d
emjayMJkim <minjeong030420@gmail.com>
2023-07-19 20:00:58 +0900

    [MOD] ViewModelFactory 수정

--- app/src/main/java/com/puzzling/puzzlingaos/util/ViewModelFactory.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/util/ViewModelFactory.kt
@@ -3,36 +3,16 @@ package com.puzzling.puzzlingaos.util
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.puzzling.puzzlingaos.data.repository.TeamRetroRepositoryImpl
 import com.puzzling.puzzlingaos.data.service.KakaoLoginService
-import com.puzzling.puzzlingaos.data.source.remote.TeamRetroDataSource
 import com.puzzling.puzzlingaos.presentation.detailRetrospect.DetailRetroViewModel
 import com.puzzling.puzzlingaos.presentation.invitationCode.InvitationCodeViewModel
 import com.puzzling.puzzlingaos.presentation.onboarding.LoginViewModel
-import com.puzzling.puzzlingaos.presentation.team.currentSituation.TeamCurrentSituationViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
-//            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
-//                RegisterViewModel(ProjectRepositoryImpl(ProjectDataSourceImpl())) as T
-//            }
             modelClass.isAssignableFrom(InvitationCodeViewModel::class.java) -> {
                 InvitationCodeViewModel(context) as T
-            }
-//            modelClass.isAssignableFrom(TeamCurrentSituationViewModel::class.java) -> {
-//                TeamCurrentSituationViewModel(
-//                    TeamCurrentSituationRepositoryImpl(
-//                        TeamCurrentSituationRemoteDataSource(),
-//                    ),
-//                ) as T
-//            }
-            modelClass.isAssignableFrom(TeamCurrentSituationViewModel::class.java) -> {
-                TeamCurrentSituationViewModel(
-                    TeamRetroRepositoryImpl(
-                        TeamRetroDataSource(),
-                    ),
-                ) as T
             }
 
             modelClass.isAssignableFrom(DetailRetroViewModel::class.java) -> {
