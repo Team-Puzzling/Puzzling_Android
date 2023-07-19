4f721b9f601a64fa1431b12a4816fa198f211e6f
gaeun <geun5744@gmail.com>
2023-07-20 00:30:30 +0900

    [FIX/#50] mvvm git 충돌 해결

--- app/src/main/java/com/puzzling/puzzlingaos/di/DataSourceModule.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/di/DataSourceModule.kt
@@ -1,16 +1,15 @@
 package com.puzzling.puzzlingaos.di
 
 import com.puzzling.puzzlingaos.data.source.remote.MyDashBoardDataSource
+import com.puzzling.puzzlingaos.data.source.remote.MyPageDataSource
 import com.puzzling.puzzlingaos.data.source.remote.ProjectDataSource
 import com.puzzling.puzzlingaos.data.source.remote.TeamReviewDataSource
 import com.puzzling.puzzlingaos.data.source.remote.WriteReviewDataSource
 import com.puzzling.puzzlingaos.data.source.remote.impl.MyDashBoardDataSourceImpl
+import com.puzzling.puzzlingaos.data.source.remote.impl.MyPageDataSourceImpl
 import com.puzzling.puzzlingaos.data.source.remote.impl.ProjectDataSourceImpl
 import com.puzzling.puzzlingaos.data.source.remote.impl.TeamReviewDataSourceImpl
-import com.puzzling.puzzlingaos.data.source.remote.MyPageDataSource
 import com.puzzling.puzzlingaos.data.source.remote.impl.WriteReviewDataSourceImpl
-import com.puzzling.puzzlingaos.data.source.remote.impl.MyPageDataSourceImpl
-
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -34,11 +33,7 @@ abstract class DataSourceModule {
 
     @Singleton
     @Binds
-    abstract fun sendProjectRegisterDataSource(DataSourceImpl: ProjectDataSourceImpl): ProjectDataSource
-
-    @Singleton
-    @Binds
-    abstract fun provideTeamRetroDataSource(DataSourceImpl: TeamReviewDataSourceImpl): TeamReviewDataSource
+    abstract fun providesTeamRetroDataSource(DataSourceImpl: TeamReviewDataSourceImpl): TeamReviewDataSource
 
     @Singleton
     @Binds
