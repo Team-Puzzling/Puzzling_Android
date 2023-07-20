f4e1b330fa445a95f1b1a7ae82be54eede4bc50c
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-20 20:50:28 +0900

    [FEAT] 홈 뷰 퍼즐판 보러가기 분기처리

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/home/personal/PersonalDashboardViewModel.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/home/personal/PersonalDashboardViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.domain.entity.ActionPlan
 import com.puzzling.puzzlingaos.domain.entity.MyPuzzleBoard
+import com.puzzling.puzzlingaos.domain.entity.PuzzleBoard
 import com.puzzling.puzzlingaos.domain.repository.MyBoardRepository
 import com.puzzling.puzzlingaos.domain.repository.WriteReviewRepository
 import com.puzzling.puzzlingaos.util.UserInfo
@@ -53,6 +55,14 @@ class PersonalDashboardViewModel @Inject constructor(
 
     private val _isSuccess = MutableLiveData(false)
     val isSuccess: LiveData<Boolean> get() = _isSuccess
+
+    val puzzleBoardList = listOf(
+        PuzzleBoard("2023.07.03. ~ 2023.07.18.", R.drawable.img_myboard1),
+        PuzzleBoard("2023.06.14. ~ 2023.06.29.", R.drawable.img_myboard2),
+        PuzzleBoard("2023.05.04. ~ 2023.06.12.", R.drawable.img_myboard3),
+        PuzzleBoard("2023.05.04. ~ 2023.06.12.", R.drawable.img_myboard4),
+
+    )
 
     init {
         getMyPuzzleData()
