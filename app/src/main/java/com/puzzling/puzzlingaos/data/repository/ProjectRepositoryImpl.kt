437f7563ef24e3492e44abb065d83d46f4ebd200
gaeun <geun5744@gmail.com>
2023-07-20 20:58:40 +0900

    [FIX/#59] ResponseDto 수정

--- app/src/main/java/com/puzzling/puzzlingaos/data/repository/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/data/repository/ProjectRepositoryImpl.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.puzzling.puzzlingaos.data.model.request.RequestInvitationCode
 import com.puzzling.puzzlingaos.data.model.request.RequestProjectRegisterDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseInvitationCodeDto
+import com.puzzling.puzzlingaos.data.model.response.ResponseJoinProjectDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseProjectRegisterDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseProjectRetroWeekDto
 import com.puzzling.puzzlingaos.data.source.remote.ProjectDataSource
@@ -27,8 +28,8 @@ class ProjectRepositoryImpl @Inject constructor(
 
     override suspend fun joinProject(
         memberId: Int,
-        request: RequestInvitationCode
-    ): ResponseInvitationCodeDto {
+        request: RequestInvitationCode,
+    ): ResponseJoinProjectDto {
         return projectDataSource.joinProject(memberId, request)
     }
 
