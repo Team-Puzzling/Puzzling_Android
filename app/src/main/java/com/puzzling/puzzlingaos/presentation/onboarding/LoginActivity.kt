d58bfa7f21f1eb60d90e035d30bc85cf57ea95e8
gaeun <geun5744@gmail.com>
2023-09-24 23:53:06 +0900

    [FEAT/#40] kakaoLogin DI 적용

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/onboarding/LoginActivity.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/onboarding/LoginActivity.kt
@@ -2,19 +2,27 @@ package com.puzzling.puzzlingaos.presentation.onboarding
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.puzzling.puzzlingaos.R
 import com.puzzling.puzzlingaos.base.BaseActivity
+import com.puzzling.puzzlingaos.data.service.KakaoAuthService
 import com.puzzling.puzzlingaos.databinding.ActivityLoginBinding
-import com.puzzling.puzzlingaos.util.ViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
-    private val viewModel: LoginViewModel by viewModels { ViewModelFactory(this) }
+    @Inject
+    lateinit var kakakoAuthService: KakaoAuthService
+
+    private val viewModel: LoginViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +32,13 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
 
     private fun startKakaoLogin() {
         binding.ibLoginKakao.setOnClickListener {
-            viewModel.kakaoLogin()
+            kakakoAuthService.startKakaoLogin(viewModel.kakaoLoginCallback)
         }
     }
 
     private fun isKakaoLoginSuccess() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.isKakaoLogin.collect { isLoginSuccess ->
                     if (isLoginSuccess) {
                         val intent =
