9a702554868a2b508bc27937d061bc28ee1a57f4
gaeun <geun5744@gmail.com>
2023-09-26 22:20:49 +0900

    [FEAT/#40] dataStore UseCase 구현

--- app/src/main/java/com/puzzling/puzzlingaos/presentation/onboarding/LoginViewModel.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/presentation/onboarding/LoginViewModel.kt
@@ -2,16 +2,23 @@ package com.puzzling.puzzlingaos.presentation.onboarding
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.auth.model.OAuthToken
 import com.puzzling.puzzlingaos.data.datasource.local.LocalDataSource
+import com.puzzling.puzzlingaos.domain.usecase.onboarding.GetTokenUseCase
+import com.puzzling.puzzlingaos.domain.usecase.onboarding.PostTokenUseCase
 import com.puzzling.puzzlingaos.util.KakaoLoginCallback
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() :
+class LoginViewModel @Inject constructor(
+    private val postTokenUseCase: PostTokenUseCase,
+    private val getTokenUseCase: GetTokenUseCase,
+) :
     ViewModel() {
     private val _isKakaoLogin = MutableStateFlow(false)
     val isKakaoLogin = _isKakaoLogin.asStateFlow()
@@ -21,6 +28,10 @@ class LoginViewModel @Inject constructor() :
             _isKakaoLogin.value = true
             Log.d("LoginViewModel", "토큰!! $token")
             LocalDataSource.setAccessToken("$token")
+            viewModelScope.launch {
+                postTokenUseCase.invoke(it)
+                Log.d("LoginViewModel", "토큰!! usecase ${getTokenUseCase.invoke()}")
+            }
         }.handleResult(token, error)
     }
 }
