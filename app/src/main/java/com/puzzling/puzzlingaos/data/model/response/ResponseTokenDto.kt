2bc00307ccb9594b693e7826c7363933da18550a
gaeun <geun5744@gmail.com>
2023-09-29 23:57:46 +0900

    [ADD/#100] Dto 추가

---
+++ app/src/main/java/com/puzzling/puzzlingaos/data/model/response/ResponseTokenDto.kt
@@ -0,0 +1,23 @@
+package com.puzzling.puzzlingaos.data.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseTokenDto(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("message")
+    val message: String,
+    @SerialName("date")
+    val data: NewToken?,
+) {
+    @Serializable
+    data class NewToken(
+        @SerialName("accessToken")
+        val accessToken: String,
+
+    )
+}
