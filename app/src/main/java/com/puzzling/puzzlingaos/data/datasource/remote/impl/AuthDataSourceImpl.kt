0abffce4063447dcbd288bdb099eb7602a720d7f
gaeun <geun5744@gmail.com>
2023-10-03 23:57:52 +0900

    [FIX/#100] 추가한 Dto에 맞춰 코드 수정

--- app/src/main/java/com/puzzling/puzzlingaos/data/datasource/remote/impl/AuthDataSourceImpl.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/data/datasource/remote/impl/AuthDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.puzzling.puzzlingaos.data.datasource.remote.impl
 
 import com.puzzling.puzzlingaos.data.datasource.remote.AuthDataSource
+import com.puzzling.puzzlingaos.data.model.request.RequestLoginDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseLoginDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseTokenDto
 import com.puzzling.puzzlingaos.data.service.AuthService
@@ -8,8 +9,7 @@ import javax.inject.Inject
 
 class AuthDataSourceImpl @Inject constructor(private val apiService: AuthService) : AuthDataSource {
     override suspend fun login(socialPlatform: String): ResponseLoginDto =
-        apiService.login(socialPlatform)
+        apiService.login(RequestLoginDto(socialPlatform))
 
     override suspend fun getToken(): ResponseTokenDto = apiService.getToken()
-
 }
--- app/src/main/java/com/puzzling/puzzlingaos/data/service/AuthService.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/data/service/AuthService.kt
@@ -1,5 +1,6 @@
 package com.puzzling.puzzlingaos.data.service
 
+import com.puzzling.puzzlingaos.data.model.request.RequestLoginDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseLoginDto
 import com.puzzling.puzzlingaos.data.model.response.ResponseTokenDto
 import retrofit2.http.Body
@@ -7,11 +8,11 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("auth")
+    @POST("api/v1/auth")
     suspend fun login(
-        @Body socialPlatform: String,
+        @Body socialPlatform: RequestLoginDto,
     ): ResponseLoginDto
 
-    @GET("auth/token")
+    @GET("api/v1/auth/token")
     suspend fun getToken(): ResponseTokenDto
 }
