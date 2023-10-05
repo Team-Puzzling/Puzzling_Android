21f8d4bec7fbea987aaa5148eb3e391549f212ce
gaeun <geun5744@gmail.com>
2023-10-05 23:16:32 +0900

    [FEAT/#100] UserDataSource 구현

--- app/src/main/java/com/puzzling/puzzlingaos/di/DataStoreModule.kt
+++ app/src/main/java/com/puzzling/puzzlingaos/di/DataStoreModule.kt
@@ -5,7 +5,9 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
 import com.puzzling.puzzlingaos.data.datasource.local.TokenDataSource
+import com.puzzling.puzzlingaos.data.datasource.local.UserDataSource
 import com.puzzling.puzzlingaos.data.entity.Token
+import com.puzzling.puzzlingaos.data.entity.User
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +29,20 @@ object DataStoreModule {
     ): DataStore<Token> {
         return DataStoreFactory.create(
             serializer = tokenDataSource,
+            produceFile = {
+                appContext.dataStoreFile(DATA_STORE_FILE_NAME)
+            },
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserDataStore(
+        @ApplicationContext appContext: Context,
+        userDataSource: UserDataSource,
+    ): DataStore<User> {
+        return DataStoreFactory.create(
+            serializer = userDataSource,
             produceFile = {
                 appContext.dataStoreFile(DATA_STORE_FILE_NAME)
             },
